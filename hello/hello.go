90884119698404e0e80d805fd0628d4c5ecaee0a
Ioannis Petrousov <petrousov@gmail.com>
2018-07-05 10:14:55 +0200

    Obligatory hello world and reverse string function

---
+++ hello/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
+
+type T struct {
+	name  string // name of the object
+	value int    // its value
+}
