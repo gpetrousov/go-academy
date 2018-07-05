62f48c62200bfbc2582bf2bcc21f3128370101c2
Ioannis Petrousov <petrousov@gmail.com>
2018-07-05 12:51:29 +0200

    Added function to read cards from file

--- cards/main.go
+++ cards/main.go
@@ -2,13 +2,14 @@ package main
 
 func main() {
 
-	cards := newDeck()
-	//
+	// cards := newDeck()
+	cards := newDeckFromFile("my_cards.txt")
+	cards.print()
 	// hand, remainingCards := deal(cards, 4)
 	//
 	// hand.print()
 	// fmt.Println("====================")
 	// remainingCards.print()
 	// fmt.Println(cards.toString())
-	cards.saveToFile("my_cards.txt")
+	// cards.saveToFile("my_cards.txt")
 }
