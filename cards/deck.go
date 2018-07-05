1514420c9efd5269d2174c4363bb6430fbb0716e
Ioannis Petrousov <petrousov@gmail.com>
2018-07-05 11:48:25 +0200

    Added typecasting and save deck to file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type of deck
 type deck []string
@@ -26,4 +30,12 @@ func newDeck() deck {
 		}
 	}
 	return cards
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 4)
-
-	hand.print()
-	fmt.Println("====================")
-	remainingCards.print()
-
+	//
+	// hand, remainingCards := deal(cards, 4)
+	//
+	// hand.print()
+	// fmt.Println("====================")
+	// remainingCards.print()
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards.txt")
 }
