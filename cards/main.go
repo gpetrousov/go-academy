052b1d5d3d84f48311c26290bf9af6ce2efc5211
Ioannis Petrousov <petrousov@gmail.com>
2018-07-05 16:58:33 +0200

    Created random shuffle function

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	// cards := newDeck()
 	cards := newDeckFromFile("my_cards.txt")
+	fmt.Println("Original hand\n======================")
+	cards.print()
+	cards.shuffle()
+	fmt.Println("Shuffled hand\n======================")
 	cards.print()
 	// hand, remainingCards := deal(cards, 4)
 	//
