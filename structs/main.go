9ccd258427c40e89f523a77392c46e807cd406ed
gpetrousov <petrousov@gmail.com>
2018-07-07 16:37:58 +0200

    Basic structs with receiver function

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,41 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
 func main() {
 
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "jim@anderson.com",
+			zipCode: 12345,
+		},
+	}
+
+	jim.print()
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+
+}
+
+// Struct receiver
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
