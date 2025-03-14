3fa6f6589cfc05f5ebc20599f3adcbf5693e250f
gpetrousov <petrousov@gmail.com>
2025-03-14 10:59:36 +0100

    Add new task

--- projects/godo/main.go
+++ projects/godo/main.go
@@ -3,61 +3,142 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"slices"
-    "os"
+	"strings"
+	"time"
 )
 
 
 type GoDoTask struct{
+    ID int
     Title string
     Description string
-    Completed bool
+    Status string
+    CreatedAt string
+    UpdateAt string
+
 }
 
 type GoDoList []GoDoTask
 
-
-var taskList GoDoList
+var usage = "Usage godo [options] [parameters]"
+var godos = "godos.json"
 
 
 func main()  {
+
+    if len(os.Args) < 2 {
+        fmt.Println(usage)
+        return
+    }
+
+    cliArgs := os.Args[1:]
+    switch cliArgs[0] {
+    case "add":
+
+        fmt.Println("==>> Add new task <<==")
+        taskTitle := cliArgs[1]
+        taskDesc := strings.Join(cliArgs[2:], " ")
+        addTask(taskTitle, taskDesc)
+
+    case "update":
+        fmt.Println("Update task")
+
+    case "delete":
+        fmt.Println("Delete task")
+
+    case "mark":
+        fmt.Println("mark task")
+
+    case "list":
+
+        switch cliArgs[1] {
+        case "all":
+            fmt.Println("==>> List all tasks <<==")
+
+        case "done":
+            fmt.Println("List all done tasks")
+
+        case "todo":
+            fmt.Println("List all todo tasks")
+
+        case "in-progress":
+            fmt.Println("List all on going tasks")
+        }
+
+    default:
+        // Print usage
+        fmt.Println(usage)
+
+    }
+
+
+
     
-    taskList = append(taskList, GoDoTask{ Title: "First task", Description: "One, Two, Three", })
-    taskList = append(taskList, GoDoTask{ Title: "Second task", Description: "One, Two, Three", })
-    // fmt.Println(taskList)
+    // taskList = append(taskList, GoDoTask{ Title: "First task", Description: "One, Two, Three", })
+    // taskList = append(taskList, GoDoTask{ Title: "Second task", Description: "One, Two, Three", })
+    // // fmt.Println(taskList)
 
-    // Append new task
-    taskList.newTask("New task", "Step one, Step two")
-
-    // List tasks
-    taskList.listTasks()
-
-    // Delete last task
-    taskList.delTask(2)
-
-    // Complete first task
-    taskList.markComplete(0)
-
-    // List tasks
-    taskList.listTasks()
-
-    // Convert to JSON
-    taskList.toJSON()
+    // // List tasks
+    // taskList.listTasks()
+    //
+    // // Delete last task
+    // taskList.delTask(2)
+    //
+    // // Complete first task
+    // taskList.markComplete(0)
+    //
+    // // List tasks
+    // taskList.listTasks()
+    //
+    // // Convert to JSON
+    // taskList.toJSON()
 }
 
-// Add new task
-func (lst *GoDoList) newTask(Title string, desc string) {
-    l := *lst
-    l = append(l, GoDoTask{ Title : Title, Description: desc})
-    *lst = l
+
+func pError(e error) {
+    log.Fatal(e)
+}
+
+// Adds new task to tasks file.
+func addTask(title, desc string) {
+
+    rb, err := os.ReadFile(godos)
+    if err != nil {
+        pError(err)
+    }
+
+    id := 0
+    var l GoDoList
+    if len(rb) != 0 {
+        json.Unmarshal(rb, &l)
+        id = len(l)
+    }
+
+    newTask := GoDoTask {
+        ID : id,
+        Title : title,
+        Description: desc,
+        Status : "godo",
+        CreatedAt: time.Now().Format(time.UnixDate),
+    }
+
+    l = append(l, newTask)
+    mb, err := json.Marshal(l)
+    if err != nil {
+        pError(err)
+    }
+    os.WriteFile(godos, mb, 0664)
 }
 
 // List tasks
 func (lst *GoDoList) listTasks()  {
     fmt.Println("#", "\tTitle",  "\t\tDescription", "\tComplete")
-    for i, t := range *lst {
-        fmt.Print(i+1, ".\t", t.Title, "\t", t.Description, "\t", t.Completed, "\n")
-    }
+    // for i, t := range *lst {
+        // fmt.Print(i+1, ".\t", t.Title, "\t", t.Description, "\t", t.Completed, "\n")
+    // }
 }
 
 // Delete task
@@ -70,16 +151,7 @@ func (lst *GoDoList) delTask(i int){
 // Mark task Completed
 func (lst *GoDoList) markComplete(i int) {
     l := *lst
-    l[i].Completed = true
+    // l[i].Completed = true
     *lst = l
 }
 
-// Convert to JSON
-func (lst *GoDoList) toJSON() {
-    l := *lst
-    b, err := json.Marshal(l)
-    if err != nil {
-        fmt.Println(err)
-    }
-    os.Stdout.Write(b)
-}
