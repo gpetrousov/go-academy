b856727eb70c11a07c1faa37c62ab1ed221dde9e
gpetrousov <petrousov@gmail.com>
2025-03-18 19:38:33 +0100

    Construct map of event for user

--- projects/ghua/main.go
+++ projects/ghua/main.go
@@ -1,5 +1,5 @@
 /*
-Output
+Output: count the actions per repository
 
 eventTYpe   repository  action  counter
 map[string]any
@@ -17,12 +17,6 @@ import (
 	"os"
 )
 
-type GitHubEvent struct {
-    Type string `json:"type"`
-    Repo json.RawMessage `json:"repo"`
-    Payload json.RawMessage `json:"payload"`
-}
-
 type GitHubRepo struct {
     Url string `json:"url"`
 }
@@ -31,26 +25,21 @@ type GitHubPayload struct {
     Action string `json:"action"`
 }
 
+type GitHubEvent struct {
+    Type string `json:"type"`
+    Repo GitHubRepo `json:"repo"`
+    Payload GitHubPayload `json:"payload"`
+}
+
 type UserActions struct {
     repository string
     action  string
     counter int
 }
 
-type PublicUserEvents map[string][]UserActions
-
 func perror(e error)  {
     if e != nil {
         log.Fatal(e)
-    }
-}
-
-func (m PublicUserEvents)contains(s string) bool {
-    _, ok := m[s]
-    if ok {
-        return true
-    } else {
-        return false
     }
 }
 
@@ -72,52 +61,49 @@ func main()  {
     err = json.Unmarshal(body, &ghEvents)
     perror(err)
 
-    var ghRepo GitHubRepo
-    var ghPayload GitHubPayload
-    publicUserEvents := make(PublicUserEvents)
+    userStats := make(map[string][]UserActions)
     for _, e := range ghEvents {
-        err = json.Unmarshal(e.Repo, &ghRepo)
-        perror(err)
-        repoUrl := ghRepo.Url
-        err = json.Unmarshal(e.Payload, &ghPayload)
-        perror(err)
-        action := ghPayload.Action
-        eventType := e.Type
-        matched := false
+        counterIncreased := false
+        // fmt.Printf("Event Type: %s\n", e.Type)
+        // fmt.Printf("Event Repo: %s\n", e.Repo.Url)
+        // fmt.Printf("Event Action: %s\n", e.Payload.Action)
+        // fmt.Println("---")
+        /*
+        add_found = False
 
-        // If type is in publicUserEvents
-        // Check if repository matches
-        // Check if action matches
-        // If all match => increase counter
-
-        // If one does not match => add UserActions struct
-
-        if publicUserEvents.contains(eventType) {
-            // Check repo match
-            for _, ua := range publicUserEvents[eventType] {
-                if ua.repository == repoUrl && ua.action == action {
-                    ua.counter += 1
-                    matched = true
+        if eventType in map
+            if repoUrl in slice AND action in slice
+                add_found = True
+                mark_counter()
+            if event_found:
+                add event to slice
+                makr
+        else
+            Add new eventType object to map
+        */
+        _, ok := userStats[e.Type]
+        if ok {
+            for i, itm := range userStats[e.Type] {
+                if (e.Repo.Url == itm.repository) && (e.Payload.Action == itm.action) {
+                    userStats[e.Type][i].counter += 1
+                    counterIncreased = true
                     break
                 }
             }
-            if !matched {
-                newUserAction := UserActions {
-                    repository: repoUrl,
-                    action: action,
-                    counter: 0,
-                }
-                publicUserEvents[eventType] = append(publicUserEvents[eventType], newUserAction)
-                matched = false
+            if !counterIncreased {
+                userStats[e.Type] = append(userStats[e.Type], UserActions{repository: e.Repo.Url, action: e.Payload.Action, counter: 1})
             }
         } else {
-            newUserAction := UserActions {
-                repository: repoUrl,
-                action: action,
-                counter: 0,
+            // Event not in map
+            newUserActions := []UserActions{
+                {
+                    repository: e.Repo.Url,
+                    action: e.Payload.Action,
+                    counter: 1,
+                },
             }
-            publicUserEvents[eventType] = append(publicUserEvents[eventType], newUserAction)
+            userStats[e.Type] = newUserActions
+            }
         }
+        fmt.Println(userStats)
     }
-    fmt.Println(publicUserEvents["PushEvent"])
-}
