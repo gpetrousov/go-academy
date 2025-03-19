a1b22010780741b15180c9594a101dba1f911b93
gpetrousov <petrousov@gmail.com>
2025-03-19 14:08:39 +0100

    Comma;ok syntax for checking a map

--- 03_map/main.go
+++ 03_map/main.go
@@ -16,6 +16,26 @@ func main() {
 	colors["blue"] = "#123456"
 
 	printMap(colors)
+
+
+    /*
+    Check if key in map - Use the comma, ok syntax
+    https://golang.cafe/blog/how-to-check-if-a-map-contains-a-key-in-go
+    */
+
+    color, ok := colors["red"]
+    if ok {
+        fmt.Printf("%s in list\n", color)
+    } else {
+        fmt.Printf("%s not in list\n", color)
+    }
+
+    color, ok = colors["cyan"]
+    if ok {
+        fmt.Printf("%s in list\n", color)
+    } else {
+        fmt.Printf("%s not in list\n", "cyan")
+    }
 }
 
 func printMap(m map[string]string) {
